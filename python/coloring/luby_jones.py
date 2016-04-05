d613bfb87f989fc9801dd27e56c63561a26d457e
leonoverweel <l.overweel@gmail.com>
2016-04-05 22:43:14 +0200

    Implement Luby-Jones graph coloring algorithm.

--- python/coloring/luby_jones.py
+++ python/coloring/luby_jones.py
@@ -2,66 +2,57 @@ import random
 
 class Coloring:
 
-	graph = None
-    
 	def __init__(self, graph=None):
 		self.graph = graph
+		self.vertices = graph.vertices
 		
-	def color(self, verify=False):
+	def color(self, verify=False, sort="id"):
 		"""Color the graph and return how many unique colors were used."""
-		
-		vertices = self.graph.vertices
-		numbers = range(0, len(vertices))
-		random.shuffle(numbers)
-		
-		# Assign number
-		for vertex in vertices:
-			self.graph.set_vertex_value(vertex, {'number': numbers.pop()})
 		
 		colored = 0
 		round = 0
+	
+		if sort == "id":
+			sorted_vertices = sorted(self.vertices, key = lambda x : x)
+		elif sort == "ascending":
+			sorted_vertices = sorted(self.vertices, key = lambda x : len(self.vertices[x].adjacent_to))
+		elif sort == "descending":
+			sorted_vertices = sorted(self.vertices, key = lambda x : -len(self.vertices[x].adjacent_to))
 		
 		# Color all vertices
-		while colored < len(vertices):
+		while colored < len(sorted_vertices):
 			
 			# Test if vertex needs to be colored this round
-			for vertex in vertices:
+			for vertex in sorted_vertices:
 				if 'color' in self.graph.get_vertex_value(vertex):
 					continue
 			
 				neighbors = self.graph.neighbors(vertex)
-				#min = vertex
-				max = vertex
+				local_max = True
 				
-				# Check if local min or max
+				# Check if local max
 				for neighbor in neighbors:
 					data_neighbor = self.graph.get_vertex_value(neighbor)
-					data_max = self.graph.get_vertex_value(max)
-					#data_min = self.graph.get_vertex_value(min)
-					
-					if not 'color' in data_max and data_neighbor['number'] > data_max['number']:
-						max = neighbor
-
-					#if not 'color' in data_min and data_neighbor['number'] < data_min['number']:
-					#	min = neighbor
-			
-				# Color if local min or max
-				if max == vertex: #or min == vertex:
+					if 'color' in data_neighbor and data_neighbor['color'] != round:
+						continue
+					if neighbor > vertex:
+						local_max = False
+				
+				# Color if local  max
+				if local_max:
 					self.graph.set_vertex_value(vertex, {u'color': round})
 					colored += 1
-			
+								
 			# Increment round
 			round += 1
 		
-		# Vertify
+		# Verify
 		correct = True
 		if verify:
-			for vertex in vertices:
-				#pass
-				#neighbors = self.graph.neighbors(vertex)
-				print self.graph.get_vertex_value(vertex)				
-				#for neighbor in neighbors:
-					#if self.graph.get_vertex_value(vertex)['color'] == self.graph.get_vertex_value(neighbor)['color']:
-						#return -1
+			for vertex in sorted_vertices:
+				neighbors = self.graph.neighbors(vertex)
+				for neighbor in neighbors:
+					if self.graph.get_vertex_value(vertex)['color'] == self.graph.get_vertex_value(neighbor)['color']:
+						return -1
 		
-		return round + 1
+		return round
