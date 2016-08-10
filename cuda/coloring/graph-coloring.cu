72ca3bd22d50e2c4fb0a6c23a172bc74e5451593
leonoverweel <l.overweel@gmail.com>
2016-08-10 14:01:26 +0200

    Read neighbors into adjacency list

--- cuda/coloring/graph-coloring.cu
+++ cuda/coloring/graph-coloring.cu
@@ -9,15 +9,13 @@ bool verbose = true;
 
 std::vector<std::vector<uint64_t>> readGraph (const std::string& path)
 {
-	std::vector<std::vector<uint64_t>> graph;
-
 	// Open the file and make sure it exists
 	std::ifstream remapStream;
 	remapStream.open(path.c_str());
 
-	if (!remapStream) return graph;
-
 	// Remap the vertex ids
+	if (verbose) std::cout << "\tRemapping vertex IDs...\n";
+
 	std::vector<std::string> lines;
 	std::string line;
 
@@ -30,7 +28,7 @@ std::vector<std::vector<uint64_t>> readGraph (const std::string& path)
 	{
 		if (line[0] == '%') 
 		{
-			if (verbose) std::cout << "\tSkipped" << std::endl;
+			if (verbose) std::cout << "\t\tSkip" << std::endl;
 			continue;
 		}
 
@@ -41,16 +39,53 @@ std::vector<std::vector<uint64_t>> readGraph (const std::string& path)
 		if (search == vertexIdMap.end())
 		{
 			vertexIdMap.insert(std::make_pair(a, highestId++));
-			if (verbose) std::cout << "\tMap " << a << " to " << highestId << std::endl;
+			if (verbose) std::cout << "\t\tMap " << a << " to " << highestId << std::endl;
 		}
 		
 		search = vertexIdMap.find(b);
 		if (search == vertexIdMap.end())
 		{
 			vertexIdMap.insert(std::make_pair(b, highestId++));
-			if (verbose) std::cout << "\tMap " << b << " to " << highestId << std::endl;
+			if (verbose) std::cout << "\t\tMap " << b << " to " << highestId << std::endl;
 		}
 	}
+
+	if (verbose) std::cout << "\tDone\n";
+
+	// Create adjacency list
+	if (verbose) std::cout << "\tCreating adjacency list...\n";
+
+	std::vector<std::vector<uint64_t>> graph(vertexIdMap.size());
+	
+	std::ifstream neighborStream;
+	neighborStream.open(path.c_str());
+
+	while (std::getline(neighborStream, line))
+	{
+		if (line[0] == '%')
+		{
+			if (verbose) std::cout << "\t\tSkip\n";
+			continue;
+		}
+
+		std::istringstream string(line);
+		string >> a >> b;
+
+		if (a != b)
+		{
+			uint64_t remappedA = vertexIdMap.find(a)->second;
+			uint64_t remappedB = vertexIdMap.find(b)->second;
+
+			graph.at(remappedA).push_back(remappedB);
+			graph.at(remappedB).push_back(remappedA);
+
+			if (verbose) std::cout << "\t\tAdd edge between "
+				<< "vertex " << remappedA << " (originally " << a << ") and "
+				<< "vertex " << remappedB << " (originally " << b << ")\n";
+		}
+	}
+
+	if (verbose) std::cout << "\tDone\n";
 
 	return graph;
 }
