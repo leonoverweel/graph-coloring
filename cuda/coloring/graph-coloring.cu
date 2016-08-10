ec1d58fa9d0b8e72cc7297d03ae203a0ac6bcedf
leonoverweel <l.overweel@gmail.com>
2016-08-10 11:58:29 +0200

    Read graph file line by line

--- cuda/coloring/graph-coloring.cu
+++ cuda/coloring/graph-coloring.cu
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main()
+std::vector<std::string> readFile( const std::string& path )
 {
-	printf("Hello, World!\n");
+	// Open the file and make sure it exists
+	std::ifstream source;
+	source.open(path.c_str());
+
+	// Iterate over the file's lines
+	std::vector<std::string> lines;
+	std::string line;
+
+	if( !source ) return lines;
+
+	while( std::getline(source, line) )
+	{
+		std::cout << line << std::endl;
+		lines.push_back(line);
+	}
+
+	return lines;
+}
+
+int main( int argc, char *argv[] )
+{
+
+	// Make sure a graph is passed
+	if( argc != 2)
+	{
+		std::cout << "No graph provided!\n";
+		return 1;
+	}
+
+	// Read the graph
+	std::string path(argv[1]);
+	std::vector<std::string> lines = readFile(path);
+
+	return 0;
 }
