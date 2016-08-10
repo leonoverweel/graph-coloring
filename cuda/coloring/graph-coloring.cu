58291aab9cc12a4abaa5cc0cf74d9055edeac62b
leonoverweel <l.overweel@gmail.com>
2016-08-10 12:47:44 +0200

    Remap vertex IDs

--- cuda/coloring/graph-coloring.cu
+++ cuda/coloring/graph-coloring.cu
@@ -3,33 +3,61 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
-std::vector<std::string> readFile( const std::string& path )
+std::vector<std::vector<uint64_t>> readGraph (const std::string& path)
 {
-	// Open the file and make sure it exists
-	std::ifstream source;
-	source.open(path.c_str());
+	std::vector<std::vector<uint64_t>> graph;
 
-	// Iterate over the file's lines
+	// Open the file and make sure it exists
+	std::ifstream remapStream;
+	remapStream.open(path.c_str());
+
+	if (!remapStream) return graph;
+
+	// Remap the vertex ids
 	std::vector<std::string> lines;
 	std::string line;
 
-	if( !source ) return lines;
+	uint64_t a, b;
+	uint64_t highestId = 0;
 
-	while( std::getline(source, line) )
+	std::unordered_map<uint64_t, uint64_t> vertexIdMap; // <original, remapped>
+	
+	while (std::getline(remapStream, line))
 	{
-		std::cout << line << std::endl;
-		lines.push_back(line);
+		if (line[0] == '%') 
+		{
+			std::cout << "Skipped" << std::endl;
+			continue;
+		}
+
+		std::istringstream string(line);
+		string >> a >> b;
+		
+		auto search = vertexIdMap.find(a);
+		if (search == vertexIdMap.end())
+		{
+			vertexIdMap.insert(std::make_pair(a, highestId++));
+			std::cout << "Map " << a << " to " << highestId << std::endl;
+		}
+		
+		search = vertexIdMap.find(b);
+		if (search == vertexIdMap.end())
+		{
+			vertexIdMap.insert(std::make_pair(b, highestId++));
+			std::cout << "Map " << b << " to " << highestId << std::endl;
+		}
 	}
 
-	return lines;
+	return graph;
 }
 
-int main( int argc, char *argv[] )
+int main (int argc, char *argv[])
 {
 
 	// Make sure a graph is passed
-	if( argc != 2)
+	if (argc != 2)
 	{
 		std::cout << "No graph provided!\n";
 		return 1;
@@ -37,7 +65,7 @@ int main( int argc, char *argv[] )
 
 	// Read the graph
 	std::string path(argv[1]);
-	std::vector<std::string> lines = readFile(path);
+	std::vector<std::vector<uint64_t>> lines = readGraph(path);
 
 	return 0;
 }
