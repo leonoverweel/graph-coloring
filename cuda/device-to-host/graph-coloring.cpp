43e637f5cfe38d54c8b1cfdd349c32eb593cc666
leonoverweel <l.overweel@gmail.com>
2016-08-12 18:12:35 +0200

    Add device to host test file

---
+++ cuda/device-to-host/graph-coloring.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+bool verbose = true;
+bool SIZE = 10;
+
+__global__
+void assign_state(uint8_t * states)
+{
+	//states[threadIdx.x] = 2;
+}
+
+int main (int argc, char *argv[])
+{
+
+    // Create host states vector
+	if (verbose) std::cout << "Creating host memory... ";
+
+	std::vector<uint8_t> * states = new std::vector<uint8_t>(SIZE);
+	int statesSize = states->size() * sizeof(uint8_t);
+
+	if (verbose) std::cout << "Done\n";
+
+	// Send data to device
+	if (verbose) std::cout << "Sending graph to device... ";
+	
+	uint8_t * deviceStatesPointer;
+	cudaMalloc((void**)&deviceStatesPointer, statesSize);
+	cudaMemcpy(deviceStatesPointer, states, statesSize, cudaMemcpyHostToDevice);
+
+	if (verbose) std::cout << "Done\n";
+
+	// Assign states
+	if (verbose) std::cout << "Assign states... ";
+
+	dim3 dimBlock(SIZE, 1);
+	dim3 dimGrid(1, 1);
+	check_if_local_max<<<dimGrid, dimBlock>>>(deviceStatesPointer);
+
+	if (verbose) std::cout << "Done\n";
+
+	// Get data back from device
+	if (verbose) std::cout << "Getting data back from device...\n";
+
+	cudaMemcpy(states, deviceStatesPointer, statesSize, cudaMemcpyDeviceToHost);
+
+	if (verbose)
+	{
+		for (int i = 0; i < states->size(); i++)
+		{
+			std::cout << "\t" << i << ": " << states->at(i) << std::endl;
+
+		}
+	}
+
+	if (verbose) std::cout << "Done\n";
+
+	return 0;
+}
